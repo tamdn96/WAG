812825fa40cfe2de550f8c69b96ec80ac0dba143
tamdn <ngoctam3096@gmail.com>
2019-10-12 23:36:19 +0700

    update url image detail report

--- app/serializers/reports/show_serializer.rb
+++ app/serializers/reports/show_serializer.rb
@@ -1,4 +1,8 @@
 class Reports::ShowSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :address, :status, :status_detect, :image_detect, :note
+  attributes :address, :status, :status_detect, :note
+  attribute :image_detect do |obj|
+    Settings.host_url + obj.image_detect.url
+  end
+
 end
