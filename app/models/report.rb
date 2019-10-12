e2c32edcb4f4eb318af82e582b4bbc6b87f4ce07
tamdn <ngoctam3096@gmail.com>
2019-10-12 17:35:25 +0700

    update code detect report

--- app/models/report.rb
+++ app/models/report.rb
@@ -12,14 +12,9 @@ class Report < ApplicationRecord
 
   enum status: [:clean, :warning, :overflow]
   enum report_type: [:report, :contribute]
+  enum status_detect: [:no , :yes]
 
   scope :_created_at_desc, -> {order(created_at: :desc)}
-  scope :today, -> {
-    where('DATE(created_at) = ?', Date.today)
-  }
-  scope :olddays, -> {
-    where('DATE(created_at) < ?', Date.today)
-  }
 
   after_create :detect_image
   accepts_nested_attributes_for :position
@@ -30,10 +25,17 @@ class Report < ApplicationRecord
     source_img = self.image.path
     fname = (Time.now.to_f * 1000).floor.to_s + source_img.split('/').last.to_s
     Dir.mkdir(Rails.root.join('tmp/detect_images')) unless Dir.exist?(Rails.root.join('tmp/detect_images'))
+    result = "#{Rails.root}/tmp/detect_images/result.txt"
     image_detected = "#{Rails.root}/tmp/detect_images/#{fname}"
-    # system(`cd lib/darknet && ./darknet detector test data/garbage-1.jpg 123 > result.txt`)
-    # # use python detect hear
-    status_detected = 'clear'
-    self.update_attributes(image_detect: File.open(source_img), status_detect: status_detected)
+    system("cd lib/darknet && ./darknet detector test #{source_img} #{image_detected.split(".")[0..-2].join(".")} > #{result}")
+    status_detected = 'no'
+    unless File.empty?(result)
+      rate = 0
+      File.open(result).each do |line|
+        rate = line.gsub("\n",'').split(' ').last.split("%")[0].to_i
+      end
+      status_detected = rate > 60 ? 'yes' : 'no'
+    end
+    self.update_attributes(image_detect: File.open(image_detected), status_detect: status_detected)
   end
 end
